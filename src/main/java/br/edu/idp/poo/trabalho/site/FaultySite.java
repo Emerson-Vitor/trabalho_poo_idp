492bd578dee029e5a84e2b796428d6ea26c953d2
Emerson Vitor Pereira da Silva <63209398+Emerson-Vitor@users.noreply.github.com>
2023-10-10 18:43:53 -0300

    Update FaultySite.java

--- src/main/java/br/edu/idp/poo/trabalho/site/FaultySite.java
+++ src/main/java/br/edu/idp/poo/trabalho/site/FaultySite.java
@@ -17,23 +17,23 @@ public class FaultySite implements SiteChecker {
     @Override
     public SiteStatus checkSite(URL url) {
         try {
-            // Simula uma verificação de site com falha
+            
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
 
-            // Adicione mais lógica específica para determinar o tipo de erro, se necessário
+            
             if (responseCode == 404) {
                 return SiteStatus.FAULTY;
             }
 
         } catch (IOException e) {
-            // Se houver uma exceção durante a conexão, considere como falha
+            
             return SiteStatus.FAULTY;
         }
 
-        // Se a resposta não for 404 ou não houver exceção, considere como funcionando
+        
         return SiteStatus.WORKING;
     }
 
